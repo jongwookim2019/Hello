e1e9398bbdf235b5c922bfdeb9b8e6b4309da1ee
user <user@YD03-02>
2019-09-20 17:50:43 +0900

    1

---
+++ HelloWorld/src/com/mycompany/kjw/HouseHold.java
@@ -0,0 +1,12 @@
+package com.mycompany.kjw;
+
+public class HouseHold {
+public static void main(String[] args) {
+	int balance =0, importation =0, expence =0, table =1;
+	Scanner scanner = new Scanner(System.in);
+	for (int i=1; i<50; i++) {
+		System.out.println(table++);
+	}
+	for 
+}
+}
